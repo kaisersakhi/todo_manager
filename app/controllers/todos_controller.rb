12b9a6d021288fe62c4fcba22e9b4ac9d85518c2
Kaiser Sakhi <kaisersakhi@gmail.com>
2023-03-20 22:21:15 +0530

    add single todo view, style for it and modified todoController and Todo model

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -2,7 +2,8 @@ class TodosController < ApplicationController
   self.skip_before_action(:verify_authenticity_token)
 
   def index
-    render plain: Todo.order(:due_date).map { |todo| todo.to_displayable_string }.join("\n")
+    # render plain: Todo.order(:due_date).map { |todo| todo.to_displayable_string }.join("\n")
+    render "index"
   end
 
   def show
@@ -13,14 +14,14 @@ class TodosController < ApplicationController
 
   def create
     todo_text = params[:todo_text]
-    due_date = DateTime.parse(params[:due_date])
+    due_date = Date.parse(params[:due_date].to_s)
     new_todo = Todo.create!(
       todo_text: todo_text,
       due_date: due_date,
       completed: false,
     )
-    reponse_text = "Hey, your new todo is created with the id:#{new_todo.id}"
-    render plain: reponse_text
+    response_text = "Hey, your new todo is created with the id:#{new_todo.id}"
+    render plain: response_text
   end
 
   #PATCH/PUT  /todos/:id
--- app/models/todo.rb
+++ app/models/todo.rb
@@ -3,4 +3,20 @@ class Todo < ActiveRecord::Base
     status = completed ? "[x]" : "[ ]"
     "#{id}. #{Date.parse(due_date)} #{todo_text} #{status}"
   end
+
+  def self.overdue
+    Todo.where("due_date < ?", Date.today.to_s)
+  end
+
+  def self.due_today
+    Todo.where("due_date = ?", Date.today.to_s)
+  end
+
+  def self.due_later
+    Todo.where("due_date > ?", Date.today.to_s)
+  end
+
+  def self.completed
+    Todo.where("completed = ?", true)
+  end
 end
