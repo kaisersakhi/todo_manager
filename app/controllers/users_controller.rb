e8ffe617c6ed3283e4c20a606bdcdea5766628e6
Kaiser Sakhi <kaisersakhi@gmail.com>
2023-03-24 10:37:16 +0530

    add render.com config

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,14 +13,20 @@ class UsersController < ApplicationController
     email = params[:email]
     password = params[:password]
 
-    user = User.create!(
+    new_user = User.new(
       first_name:,
       last_name:,
       email:,
       password:
     )
-    session[:current_user_id] = user.id
-    redirect_to '/'
+
+    if new_user.save
+      session[:current_user_id] = new_user.id
+      redirect_to todos_path
+    else
+      flash[:error] = new_user.errors.full_messages.join(', ')
+      redirect_to new_user_path
+    end
   end
 
   def login
